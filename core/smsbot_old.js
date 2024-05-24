93b7f158b7f40e7f573dcb7283f822942f4a8c3d
Dev Verma <dev@obto.co>
2024-05-23 23:53:36 -0400

    Update smsbot_old.js

--- core/smsbot_old.js
+++ core/smsbot_old.js
@@ -2,8 +2,8 @@
  * Created by divyanshverma on 10/22/15.
  */
 // Your accountSid and authToken from twilio.com/user/account
-var accountSid = 'AC1d94aca36cb1c20f58ddb80312e9208f';
-var authToken = "b48c94cc5c85736255fd2f6a3ff0795d";
+var accountSid = '';
+var authToken = "";
 var client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
